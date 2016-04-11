ad4808ee3a9179d4cb51ee7ea0b3996f7ec2a867
Rickx777 <Cwong7@stuy.edu>
2016-04-10 21:57:41 -0400

    Activity 4 completed

--- MagpieActivityStarterCode/activity4/Magpie4.java
+++ MagpieActivityStarterCode/activity4/Magpie4.java
@@ -54,6 +54,17 @@ public class Magpie4
 			response = transformIWantToStatement(statement);
 		}
 
+		else if (findKeyword(statement, "I want", 0) >= 0)
+		    {
+			response = transformIWantStatement(statement);
+		    }
+
+		else if (findKeyword(statement, "I", 0) >= 0 &&
+			 findKeyword(statement, "you", findKeyword(statement,"I",0)) >= 0)
+		    {
+			response = transformIYouStatement(statement);
+		    }
+
 		else
 		{
 			// Look for a two word (you <something> me)
@@ -95,6 +106,21 @@ public class Magpie4
 		return "What would it mean to " + restOfStatement + "?";
 	}
 
+        private String transformIWantStatement(String statement)
+	{
+		//  Remove the final period, if there is one
+		statement = statement.trim();
+		String lastChar = statement.substring(statement
+				.length() - 1);
+		if (lastChar.equals("."))
+		{
+			statement = statement.substring(0, statement
+					.length() - 1);
+		}
+		int psn = findKeyword (statement, "I want", 0);
+		String restOfStatement = statement.substring(psn + 6).trim();
+		return "Would you really be happy if you had " + restOfStatement + "?";
+	}
 	
 	
 	/**
@@ -121,7 +147,26 @@ public class Magpie4
 		String restOfStatement = statement.substring(psnOfYou + 3, psnOfMe).trim();
 		return "What makes you think that I " + restOfStatement + " you?";
 	}
-	
+
+
+        private String transformIYouStatement(String statement)
+        {
+		//  Remove the final period, if there is one
+		statement = statement.trim();
+		String lastChar = statement.substring(statement
+				.length() - 1);
+		if (lastChar.equals("."))
+		{
+			statement = statement.substring(0, statement
+					.length() - 1);
+		}
+		
+		int psnOfI = findKeyword (statement, "I", 0);
+		int psnOfYou = findKeyword (statement, "you", psnOfI + 1);
+		
+		String restOfStatement = statement.substring(psnOfI + 1, psnOfYou).trim();
+		return "What makes you think that you " + restOfStatement + " me?";
+	}
 	
 
 	
